b2b47c1bdedca9b9dd381fc9b617a9cba9692996
robertatena <roberta.tena@adesampa.com.br>
2025-11-07 15:46:44 -0300

    Delete gi e gui directory

--- gi e gui/tailwind.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: { fontFamily: { serif: ["ui-serif","Georgia","serif"] } } },
-  plugins: [],
-};
